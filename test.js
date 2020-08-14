f5f915b9cf2a8ffdd24d08ab3f964bb3dfd9aad8
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-08-14 16:58:06 +0530

    Learing Variable on Js Done

---
+++ test.js
@@ -0,0 +1,7 @@
+var fullName="Hardik Gupta";
+var contact=8982840527;
+var contactString="8982840527";
+let email="parnami.hardik50@gmail.com";
+const bool=true;
+//this is comment learing js
+console.log(fullName,contact,contactString,email,bool);
