86e45a4aebfce88b86c0e1176b892b38a9478d0c
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-08-14 18:05:34 +0530

    Understanding Function in Js

---
+++ function.js
@@ -0,0 +1,11 @@
+function nameAndSum(name,numOne,numTwo) { //function and its parameter
+    console.log("Hey",name); 
+    console.log(`Your Sum is ${numOne+numTwo}`); //Backtick allow to use variable inside string in a defind way
+}
+
+
+
+var name="Guest"
+let numOne=10;
+let numTwo=20;
+nameAndSum(name,numOne,numTwo); //Function calling
