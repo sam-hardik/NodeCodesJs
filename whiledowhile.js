90c37e22c5dd877864fb6a1ffe20d0ed1aae48ff
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-08-16 19:48:03 +0530

    While And Do While Control Statements

---
+++ whiledowhile.js
@@ -0,0 +1,18 @@
+var mList=[
+    "Asia",
+    "India",
+    "Madhya Pradesh",
+    "Dewas",
+    "Khategaon",
+    455336
+];
+let index=0;
+while (index<mList.length) {
+    console.log(mList[index]);
+    index+=1;
+}
+let i=1;
+do{
+    console.log(i**2);
+    i++;
+}while(i<=10)
