07e8e44771ff0721dbec9319571a7bee3f78a46f
HARDIK GUPTA <parnami.hardik50@gmail.com>
2020-08-16 20:48:19 +0530

    For Each loop Concept

---
+++ forEach.js
@@ -0,0 +1,16 @@
+var mList=[
+    "Asia",
+    "India",
+    "Madhya Pradesh",
+    "Dewas",
+    "Khategaon",
+    455336
+];
+
+mList.forEach(element => { //require callback
+    console.log(element);
+}); //forEach loops through each element of the array or list
+//or
+List.forEach((element) => console.log(element));
+//or
+mList.forEach((element) => (console.log(element)));
